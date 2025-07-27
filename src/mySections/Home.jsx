01700d461e04ce675cf10165ccc82d62da4aee6a
Roch APETOH D. <rapetoh@hotmail.com>
2025-07-27 10:22:23 -0500

    Made changes to my portfolio and especially to the Resume jsx file to allow potentials recrutiers to see quicker my background and work experience

--- src/mySections/Home.jsx
+++ src/mySections/Home.jsx
@@ -4,15 +4,18 @@ import available from '../assets/images/available.jpeg'
 
 const Home = () => {
   return (
-    <div id='home' className='h-screen pt-20 flex flex-row max-md:flex-col max-md:px-1 justify-center items-center px-40 max-lg:px-32'>
+    <div id='home' className='h-screen/2 pt-20 flex flex-row max-md:flex-col max-md:px-1 justify-center items-center px-40 max-lg:px-32'>
 
       <div className='flex flex-col gap-2 w-2/5'>
-        <marquee className="w-4/5 text-zinc-500 max-md:w-full" behavior="" scrollamount="5" direction="left">Full-Stack Software Development - Database Management - Data Analysis </marquee>
+        <marquee className="w-4/5 text-zinc-500 max-md:w-full" behavior="" scrollamount="5" direction="left">Full-Stack Software Development - Database Management</marquee>
         <div className='flex flex-row gap items-center gap-1 max-lg:text-sm'>
-          <img className='w-8 h-8 rounded-md max-lg:w-5 max-lg:h-5' src={available} alt="" />&nbsp; <div className='rounded-full w-2 h-2 bg-green-500'></div>  &nbsp; Available to work
+          <img className='w-7 h-7 rounded-md max-lg:w-5 max-lg:h-5' src={available} alt="" />&nbsp; <div className='rounded-full w-2 h-2 bg-green-500'></div>  &nbsp; Available to work
         </div>
-        <div className='text-4xl font-bold text-white max-md:text-center max-md:text-2xl max-md:w-full w-4/5'>
+        <div className='text-2xl font-bold text-white max-md:text-center max-md:text-2xl max-md:w-full w-3/5'>
           Hi, I'm Roch. <br /> A Software Engineer
+        </div>
+        <div className='text-sm text-zinc-500 max-md:text-xs max-md:text-center max-md:w-full w-4/5'>
+          I’m a Software Engineer with 2+ years of experience and a strong focus on building solutions that solve real-world problems. My tools are the MERN Stack, JAVA-Spring Boot, PHP, Laravel. My ability and passion to turn a problem into a solution is what mainly drive me in everything I build or everywhere I work.
         </div>
         <div className='flex flex-row gap-2 max-md:flex-col max-md:gap-1'>
           {/* <button onClick={null} className='flex flex-row justify-center items-center gap-3 my-3 p-3 bg-[#57c6e5] rounded-xl hover:bg-white transform duration-200'>
@@ -22,8 +25,8 @@ const Home = () => {
             </svg>
           </button> */}
           <a href="#project" className='max-lg:m-auto'>
-            <button onClick={null} className='flex flex-row justify-center items-center gap-3 my-3 p-3 bg-zinc-600 rounded-xl hover:bg-zinc-400 transform duration-200'>
-              <span className='text-white font-bold'>Scroll Down</span>
+            <button onClick={null} className='flex flex-row justify-center items-center gap-3 my-1 p-2 bg-zinc-600 rounded-xl hover:bg-zinc-400 transform duration-200'>
+              <span className='text-white font-semibold'>Scroll Down</span>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" class="size-4" >
                 <path fill-rule="evenodd" d="M11.47 13.28a.75.75 0 0 0 1.06 0l7.5-7.5a.75.75 0 0 0-1.06-1.06L12 11.69 5.03 4.72a.75.75 0 0 0-1.06 1.06l7.5 7.5Z" clip-rule="evenodd" />
                 <path fill-rule="evenodd" d="M11.47 19.28a.75.75 0 0 0 1.06 0l7.5-7.5a.75.75 0 1 0-1.06-1.06L12 17.69l-6.97-6.97a.75.75 0 0 0-1.06 1.06l7.5 7.5Z" clip-rule="evenodd" />
@@ -34,7 +37,7 @@ const Home = () => {
       </div>
 
 
-      <div className='rounded-3xl p-0 mb-4 bg-gradient-to-t from-[#57c6e5] via-[#57c6e5]/10 to-[#40E0D0]/0 w-2/5 max-lg:w-4/5'>
+      <div className='rounded-3xl p-0 mb-4 bg-gradient-to-t from-[#57c6e5] via-[#57c6e5]/10 to-[#40E0D0]/0 w-1/5 max-lg:w-2/5'>
         <img className='' src={rochProfile} alt="" />
       </div>
     </div>
--- src/mySections/Resume.jsx
+++ src/mySections/Resume.jsx
@@ -20,18 +20,18 @@ const Resume = () => {
   )
 
   return (
-    <div id='resume' className='h-auto rounded-3xl pt-16 mx-32 max-lg:mx-12'>
+    <div id='resume' className='h-auto rounded-3xl pt-6 mx-32 max-lg:mx-12'>
 
       {
         windowWidth > 768 ?
 
-          <div className='grid grid-cols-2 grid-rows-2 '>
+          <div className='grid grid-cols-2 grid-rows-2 grid-rows-[75px_auto_auto_auto_auto_auto]'>
 
-            <div className='col-start-1 row-start-1'>
+            <div className='col-start-1 row-start-1 h-1 m-0 p-0'>
               <h2 className='text-3xl font-bold text-center'>Education</h2>
             </div>
 
-            <div className='col-start-2 row-start-1'>
+            <div className='col-start-2 row-start-1 h-1 m-0 p-0'>
               <h2 className='text-3xl font-bold text-center'>Work Experience</h2>
             </div>
 
@@ -47,13 +47,17 @@ const Resume = () => {
               <div className=' size-4 rounded-full bg-[#57c6e5] my-[70px] mx-[-7px]'> </div>
             </div>
 
-            <div className=' col-start-1 row-start-2 border-r-2 border-[#57c6e5] m-[-1px] pr-7' data-aos="fade-up" data-aos-duration="500" data-aos-offset="70">
+            <div className=' col-start-1 row-start-2 border-r-2 border-[#57c6e5] m-[-1px] pr-7' data-aos="fade-up" data-aos-duration="500" data-aos-offset="40">
               <div className='border-2 rounded-3xl p-5 border-zinc-700 hover:bg-zinc-800 transform duration-200'>
 
                 <h2 className='text-[#57c6e5] text-md font-bold font-palanquin'>Second Bachelor Degree</h2>
                 <p className='text-zinc-300 text-md font-semibold font-palanquin'>Major: Computer science</p>
                 <p className='text-zinc-300 font-bold font-palanquin'>2025 - 2026</p>
-                <p className='text-zinc-400'>University of Maryland Global Campus | USA, ML</p>
+                <p className='text-zinc-400'>University of Maryland Global Campus | USA, ML</p><br />
+
+                <p className='text-zinc-400'>- Building on my first Bachelor's in Software Engineering and industry experience, I’m pursuing a second Bachelor's in Computer Science at UMGC,</p>
+                <p className='text-zinc-400'>- Dedicate 40+ hours/week to mastering cloud computing, networking, and software engineering principles aligned with U.S. industry standards,</p>
+                <p className='text-zinc-400'>- Completed 150+ hours of hands-on labs and projects, maintaining an A grade and improving app performance and deployment reliability in personal builds.</p>
 
               </div>
             </div>
@@ -74,14 +78,13 @@ const Resume = () => {
               <div className=' size-4 rounded-full bg-[#57c6e5] my-[70px] mx-[-7px]'> </div>
             </div>
 
-            <div className=' col-start-2 row-start-3 border-l-2 border-[#57c6e5] m-[-1px] pl-7 pb-6' data-aos="fade-up" data-aos-duration="500" data-aos-offset="70">
+            <div className=' col-start-2 row-start-3 border-l-2 border-[#57c6e5] m-[-1px] pl-7 pb-6' data-aos="fade-up" data-aos-duration="500" data-aos-offset="40">
               <div className=' border-2 rounded-3xl p-5 border-zinc-700 hover:bg-zinc-800 transform duration-200'>
                 <h2 className='text-md text-[#57c6e5] font-bold font-palanquin'>Independant Software Engineer</h2>
                 <p className='text-zinc-300'>Aug 2024 - Present</p>
-                <p className='text-zinc-400'>- Explored JAVA, MERN Stack and API integration to build fully functional full-stack features.</p>
-                <p className='text-zinc-400'>- Focused on performance optimization, UI/UX, and responsive design.</p>
-                <p className='text-zinc-400'>- Received positive feedback from peers and local users who tested or used early versions.</p>
-                <p className='text-zinc-400'>- Applied Git version control, CI|CD concepts and Agile-like planning for self-managed sprints.</p>
+                <p className='text-zinc-400'>- Built and deployed full-stack features using Java, MERN Stack, and Spring Boot, reducing code redundancy by 30% through reusable API and component design.</p>
+                <p className='text-zinc-400'>- Designed and executed 4+ self-managed development sprints using Git and Trello, achieving 100% task completion each cycle.</p>
+                <p className='text-zinc-400'>- Received positive 4/5 global feedback from peers and local users who tested or used early versions of applications.</p>
 
                 <div className='flex flex-row flex-wrap gap-2'>
                   <div className='rounded-xl w-8 p-1 m-1 bg-zinc-400 max-lg:h-10'>
@@ -123,16 +126,18 @@ const Resume = () => {
               <div className=' size-4 rounded-full bg-[#57c6e5] my-[70px] mx-[-7px]'> </div>
             </div>
 
-            <div className=' col-start-2 row-start-4 border-l-2 border-[#57c6e5] m-[-1px] pl-7' data-aos="fade-up" data-aos-duration="500" data-aos-offset="70">
+            <div className=' col-start-2 row-start-4 border-l-2 border-[#57c6e5] m-[-1px] pl-7' data-aos="fade-up" data-aos-duration="500" data-aos-offset="40">
               <div className=' border-2 rounded-3xl p-5 border-zinc-700 hover:bg-zinc-800 transform duration-200'>
                 <h2 className='text-md text-[#57c6e5] font-bold font-palanquin'>Full-Stack Software Developer</h2>
                 <p className='text-zinc-300'>Suisco - ITPlex Consult | Aug 2023 - Aug 2024</p>
-                <p className='text-zinc-400'>- Developed a biometric identification solution from start to finish.</p>
-                <p className='text-zinc-400'>- Built core logic using Python, PHP and SQL, integrated with fingerprint reader hardware.</p>
-                <p className='text-zinc-400'>- Used Python to implement facial recognition.</p>
-                <p className='text-zinc-400'>- Successfully verified identity with 98% accuracy in test scenarios.</p>
-                <p className='text-zinc-400'>- Deployed locally in a fintech environment and satisfied 90% of expected results.</p>
-                <p className='text-zinc-400'>- Enhanced security, identity management, and onboarding speed by 70%.</p>
+
+
+                <p className='text-zinc-400'>- Led development of biometric identification system, verifying identities with 98% accuracy in internal tests.</p>
+                <p className='text-zinc-400'>- Coded backend with Python, PHP, and SQL, and integrated with fingerprint hardware, reducing identification fraud attempts by 75%.</p>
+                <p className='text-zinc-400'>- Deployed the solution in a fintech environment, meeting 90% of project KPIs and reducing onboarding time by 50%.</p>
+                <p className='text-zinc-400'>- Improved user identity lifecycle with new logic, boosting security and onboarding efficiency by 70%.</p>
+
+
                 <div className='flex flex-row flex-wrap gap-2'>
                   <div className='rounded-xl w-8 p-1 m-1 bg-zinc-400 max-lg:h-10'>
                     <img className='w-6' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
@@ -175,13 +180,17 @@ const Resume = () => {
 
 
 
-            <div className='col-start-1 row-start-5 border-r-2 border-[#57c6e5] pr-7' data-aos="fade-up" data-aos-duration="500" data-aos-offset="70">
+            <div className='col-start-1 row-start-5 border-r-2 border-[#57c6e5] pr-7' data-aos="fade-up" data-aos-duration="500" data-aos-offset="40">
               <div className='border-2 rounded-3xl p-5 border-zinc-700 hover:bg-zinc-800 transform duration-200'>
 
                 <h2 className='text-[#57c6e5] text-md font-bold font-palanquin'>Bachelor of Science in Computer Science</h2>
                 <p className='text-zinc-300 text-md font-semibold font-palanquin'>Major: Software Engineering and Information Systems</p>
                 <p className='text-zinc-300 font-bold font-palanquin'>2020 - 2024</p>
-                <p className='text-zinc-400'>IAI-TOGO | Lomé-Togo</p>
+                <p className='text-zinc-400'>IAI-TOGO | Lomé-Togo</p> <br/>
+
+                <p className='text-zinc-400'> - Completed a comprehensive program focused on software engineering, information systems, and programming analysis.</p>
+                <p className='text-zinc-400'>- Valedictorian of the Class of 2024,</p>
+                <p className='text-zinc-400'>- Certified by WES (World Education Services) with an equivalent GPA: 3.82/4.0</p>
 
               </div>
             </div>
@@ -200,7 +209,7 @@ const Resume = () => {
 
 
 
-            <div className='col-start-2 row-start-6 border-l-2 border-[#57c6e5] m-[-1px] pl-7' data-aos="fade-up" data-aos-duration="500" data-aos-offset="70">
+            <div className='col-start-2 row-start-6 border-l-2 border-[#57c6e5] m-[-1px] pl-7' data-aos="fade-up" data-aos-duration="500" data-aos-offset="40">
               <div className='border-2 rounded-3xl p-5 border-zinc-700 hover:bg-zinc-800 transform duration-200'>
                 <h2 className='text-md text-[#57c6e5] font-bold font-palanquin'>Flutter Mobile & Desktop Developer | Intern</h2>
                 <p className='text-zinc-300'>SIB Sarl | June 2023 - Aug 2023</p>
@@ -244,14 +253,13 @@ const Resume = () => {
             </div>
 
 
-            <div className='' data-aos="fade-up" data-aos-duration="500" data-aos-offset="70">
+            <div className='' data-aos="fade-up" data-aos-duration="500" data-aos-offset="40">
               <div className=' border-2 rounded-3xl p-5 border-zinc-700 hover:bg-zinc-800 transform duration-200'>
                 <h2 className='text-md text-[#57c6e5] font-bold font-palanquin'>Independant Software Engineer</h2>
                 <p className='text-zinc-300'>Aug 2024 - Present</p>
-                <p className='text-zinc-400'>- Explored JAVA, MERN Stack and API integration to build fully functional full-stack features.</p>
-                <p className='text-zinc-400'>- Focused on performance optimization, UI/UX, and responsive design.</p>
-                <p className='text-zinc-400'>- Received positive feedback from peers and local users who tested or used early versions.</p>
-                <p className='text-zinc-400'>- Applied Git version control, CI|CD concepts and Agile-like planning for self-managed sprints.</p>
+                <p className='text-zinc-400'>- Built and deployed full-stack features using Java, MERN Stack, and Spring Boot, reducing code redundancy by 30% through reusable API and component design.</p>
+                <p className='text-zinc-400'>- Designed and executed 4+ self-managed development sprints using Git and Trello, achieving 100% task completion each cycle.</p>
+                <p className='text-zinc-400'>- Received positive 4/5 global feedback from peers and local users who tested or used early versions of applications.</p>
 
                 <div className='flex flex-row flex-wrap gap-2'>
                   <div className='rounded-xl w-8 p-1 m-1 bg-zinc-400 max-lg:h-10'>
@@ -279,16 +287,14 @@ const Resume = () => {
               </div>
             </div>
 
-            <div className='' data-aos="fade-up" data-aos-duration="500" data-aos-offset="70">
+            <div className='' data-aos="fade-up" data-aos-duration="500" data-aos-offset="40">
               <div className=' border-2 rounded-3xl p-5 border-zinc-700 hover:bg-zinc-800 transform duration-200'>
                 <h2 className='text-md text-[#57c6e5] font-bold font-palanquin'>Full-Stack Software Developer</h2>
                 <p className='text-zinc-300'>Suisco - ITPlex Consult | Aug 2023 - Aug 2024</p>
-                <p className='text-zinc-400'>- Developed a biometric identification solution from start to finish.</p>
-                <p className='text-zinc-400'>- Built core logic using Python, PHP and SQL, integrated with fingerprint reader hardware.</p>
-                <p className='text-zinc-400'>- Used Python to implement facial recognition.</p>
-                <p className='text-zinc-400'>- Successfully verified identity with 98% accuracy in test scenarios.</p>
-                <p className='text-zinc-400'>- Deployed locally in a fintech environment and satisfied 90% of expected results.</p>
-                <p className='text-zinc-400'>- Enhanced security, identity management, and onboarding speed by 70%.</p>
+                <p className='text-zinc-400'>- Led development of biometric identification system, verifying identities with 98% accuracy in internal tests.</p>
+                <p className='text-zinc-400'>- Coded backend with Python, PHP, and SQL, and integrated with fingerprint hardware, reducing identification fraud attempts by 75%.</p>
+                <p className='text-zinc-400'>- Deployed the solution in a fintech environment, meeting 90% of project KPIs and reducing onboarding time by 50%.</p>
+                <p className='text-zinc-400'>- Improved user identity lifecycle with new logic, boosting security and onboarding efficiency by 70%.</p>
                 <div className='flex flex-row flex-wrap gap-2'>
                   <div className='rounded-xl w-8 p-1 m-1 bg-zinc-400 max-lg:h-10'>
                     <img className='w-6' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
@@ -323,7 +329,7 @@ const Resume = () => {
 
 
 
-            <div className='' data-aos="fade-up" data-aos-duration="500" data-aos-offset="70">
+            <div className='' data-aos="fade-up" data-aos-duration="500" data-aos-offset="40">
               <div className='border-2 rounded-3xl p-5 border-zinc-700 hover:bg-zinc-800 transform duration-200'>
                 <h2 className='text-md text-[#57c6e5] font-bold font-palanquin'>Flutter Mobile & Desktop Developer | Intern</h2>
                 <p className='text-zinc-300'>SIB Sarl | June 2023 - Aug 2023</p>
@@ -353,7 +359,7 @@ const Resume = () => {
             </div>
 
 
-            <div className='' data-aos="fade-up" data-aos-duration="500" data-aos-offset="70">
+            <div className='' data-aos="fade-up" data-aos-duration="500" data-aos-offset="40">
               <div className='border-2 rounded-3xl p-5 border-zinc-700 hover:bg-zinc-800 transform duration-200'>
 
                 <h2 className='text-[#57c6e5] text-md font-bold font-palanquin'>Second Bachelor Degree</h2>
@@ -363,13 +369,18 @@ const Resume = () => {
 
               </div>
             </div>
-            <div className='' data-aos="fade-up" data-aos-duration="500" data-aos-offset="70">
+            <div className='' data-aos="fade-up" data-aos-duration="500" data-aos-offset="40">
               <div className='border-2 rounded-3xl p-5 border-zinc-700 hover:bg-zinc-800 transform duration-200'>
 
                 <h2 className='text-[#57c6e5] text-md font-bold font-palanquin'>Bachelor of Science in Computer Science</h2>
                 <p className='text-zinc-300 text-md font-semibold font-palanquin'>Major: Software Engineering and Information Systems</p>
                 <p className='text-zinc-300 font-bold font-palanquin'>2020 - 2024</p>
-                <p className='text-zinc-400'>IAI-TOGO | Lomé-Togo</p>
+                <p className='text-zinc-400'>IAI-TOGO | Lomé-Togo</p><br/>
+
+                <p className='text-zinc-400'> - Completed a comprehensive program focused on software engineering, information systems, and programming analysis.</p>
+                <p className='text-zinc-400'>- Valedictorian of the Class of 2024,</p>
+                <p className='text-zinc-400'>- Certified by WES (World Education Services) with an equivalent GPA: 3.82/4.0</p>
+
 
               </div>
             </div>
